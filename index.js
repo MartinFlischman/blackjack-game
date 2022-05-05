7c421d972c9c824e0ae0a27db7be77681f9ac512
Martin Flischman <mflischman@icloud.com>
2022-05-05 08:53:40 +0200

    Random cards
    - A Function was added so that a random card is drawn/added to the game, each time the player clicks on "Start Game" and "New Card".

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let firstCard = 10;
-let secondCard = 4;
+let firstCard = getRandomCard();
+let secondCard = getRandomCard();
 let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
@@ -8,6 +8,17 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+
+function getRandomCard() {
+	let randomNumer = Math.floor(Math.random() * 13) + 1;
+	if (randomNumer > 10) {
+		return 10;
+	} else if (randomNumer === 1) {
+		return 11;
+	} else {
+		return randomNumer;
+	}
+}
 
 function startGame() {
 	renderGame();
@@ -33,9 +44,8 @@ function renderGame() {
 }
 
 function newCard() {
-	let card = 6;
+	let card = getRandomCard();
 	sum += card;
 	cards.push(card);
-	console.log(cards);
 	renderGame();
 }
