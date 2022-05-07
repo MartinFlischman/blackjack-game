1e3fe3986b6815182a68f850dfbf5ff73c83f842
Martin Flischman <mflischman@icloud.com>
2022-05-07 09:12:30 +0200

    Game features
    - Added functionality for a New card to be drawn only if the player hasn't already won nor is out of the game,
    - Added the player's name and amount of chips to display.

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+let player = {
+	name: "Martin",
+	chips: 145,
+};
 let cards = [];
 let sum = 0;
 let hasBlackJack = false;
@@ -6,6 +10,9 @@ let message = "";
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let playerEl = document.getElementById("player-el");
+
+playerEl.textContent += ` ${player.name} - $${player.chips}`;
 
 function getRandomCard() {
 	let randomNumber = Math.floor(Math.random() * 13) + 1;
@@ -47,8 +54,10 @@ function renderGame() {
 }
 
 function newCard() {
-	let card = getRandomCard();
-	sum += card;
-	cards.push(card);
-	renderGame();
+	if (isAlive === true && hasBlackJack === false) {
+		let card = getRandomCard();
+		sum += card;
+		cards.push(card);
+		renderGame();
+	}
 }
